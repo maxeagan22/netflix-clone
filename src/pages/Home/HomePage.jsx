a67d5c37da0d08771227ea2ed8b6d33e21d31ba8
Max Eagan <meagan7654@gmail.com>
2024-07-29 18:42:15 -0500

    Update file structue
    Update file structure for project

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>HomePage</div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/Player/PlayerPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PlayerPage.css'
+
+const PlayerPage = () => {
+  return (
+    <div className='playerpage'>PlayerPage</div>
+  )
+}
+
+export default PlayerPage

978552a0335661c734628e0bc94f99873f1134ec
Max Eagan <meagan7654@gmail.com>
2024-08-02 19:36:51 -0500

    Display trailers on playerpage

--- src/pages/Player/PlayerPage.jsx
+++ src/pages/Player/PlayerPage.jsx
@@ -1,20 +1,56 @@
-import React from 'react'
-import './PlayerPage.css'
-import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import React, { useEffect, useState } from 'react';
+import './PlayerPage.css';
+import back_arrow_icon from '../../assets/back_arrow_icon.png';
+import { useParams } from 'react-router-dom';
 
 const PlayerPage = () => {
+
+  const { id } = useParams();
+
+  const [data, setData] = useState({
+    name: '',
+    key: '',
+    published_at: '',
+    type: ''
+  });
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNzhhNzMzNDM5ZWMwYWQ5N2NhZTZiYzhhYjA3Zjk4MCIsIm5iZiI6MTcyMjU2MjMyMS45MzM1NSwic3ViIjoiNjZhYzM1ZmY5ZDYxNTFjOWU3NmMyYTUzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.PkDJcS3zKwfOPhngPS-OgczDXwkMsYFEuEmqcSOcnZw'
+      }
+    };
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(response => response.json())
+      .then(response => {
+        if (response.results && response.results.length > 0) {
+          setData(response.results[0]);
+        } else {
+          console.error('No results found');
+        }
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className='playerpage'>
       <img src={back_arrow_icon} alt=''/>
-      <iframe width='90%' height='90%' src='http://www.youtube.com/embed/Tn6-PIqc4UM' title='trailer' frameBorder='0' allowFullScreen>
-        <div className="player-info">
-          <p>Published Date</p>
-          <p>Name</p>
-          <p>Type</p>
-        </div>
-      </iframe>
+      <iframe width='90%' height='90%'
+       src={`https://youtube.com/embed/${data.key}`}
+       title='trailer'
+       frameBorder='0'
+       allowFullScreen 
+       />
+       <div className="player info">
+        <p>{data.published_at.slice(0,10)}</p>
+        <p>{data.name}</p>
+        <p>{data.type}</p>
+      </div> 
     </div>
-  )
+  );
 }
 
-export default PlayerPage
+export default PlayerPage;

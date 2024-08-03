7404d0b44157ed96800438d8c2d3682b3c5fdeb0
Max Eagan <meagan7654@gmail.com>
2024-08-02 20:05:23 -0500

    Refactor Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,42 @@
-import React from 'react'
-import './Footer.css'
-import youtube_icon from '../../assets/youtube_icon.png'
-import twitter_icon from '../../assets/twitter_icon.png'
-import instagram_icon from '../../assets/instagram_icon.png'
-import facebook_icon from '../../assets/facebook_icon.png'
+import React from 'react';
+import './Footer.css';
+import youtubeIcon from '../../assets/youtube_icon.png';
+import twitterIcon from '../../assets/twitter_icon.png';
+import instagramIcon from '../../assets/instagram_icon.png';
+import facebookIcon from '../../assets/facebook_icon.png';
+
+const SocialMediaIcons = () => (
+  <div className="footer-icons">
+    <img src={facebookIcon} alt="Facebook Icon" />
+    <img src={instagramIcon} alt="Instagram Icon" />
+    <img src={twitterIcon} alt="Twitter Icon" />
+    <img src={youtubeIcon} alt="YouTube Icon" />
+  </div>
+);
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className="footer-icons">
-        <img src={facebook_icon} alt=''/>
-        <img src={instagram_icon} alt=''/>
-        <img src={twitter_icon} alt=''/>
-        <img src={youtube_icon} alt=''/>
-      </div>
+    <div className="footer">
+      <SocialMediaIcons />
       <ul>
         <li>Audio Description</li>
         <li>Help Center</li>
         <li>Gift Cards</li>
-        <li>Media Center </li>
+        <li>Media Center</li>
         <li>Investor Relations</li>
         <li>Jobs</li>
         <li>Terms of Use</li>
         <li>Privacy</li>
         <li>Legal Notices</li>
         <li>Cookie Preferences</li>
-        <li>Corportate Information</li>
+        <li>Corporate Information</li>
         <li>Contact Us</li>
       </ul>
-      <p className='copyright-text'>
-      © 1997-2023 Netflix, Inc.
+      <p className="copyright-text">
+        © 1997-2023 Netflix, Inc.
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

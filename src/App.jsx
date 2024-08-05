ed6dba604853d0606a0b6877ca031feaefa81dac
Max Eagan <meagan7654@gmail.com>
2024-08-04 21:46:53 -0500

    Redirect user to to appropriate page based on login state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import Home from './pages/Home/HomePage'
 import Login from './pages/Login/LoginPage'
 import PlayerPage from './pages/Player/PlayerPage';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 export const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if(user){
+        console.log("Logged In");
+        navigate('/')
+      }
+      else{
+        console.log("Logged Out");
+        navigate('/login')
+      }
+    }) 
+  }, [])
+
   return (
     <div>
       <Routes>

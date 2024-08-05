37ee3fbaa080ff6714fbe3e081f35a725c2048ea
Max Eagan <meagan7654@gmail.com>
2024-08-04 22:00:57 -0500

    Add new style for loginpage error

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect } from 'react';
 import { auth } from './firebase';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
 
@@ -25,6 +27,7 @@ export const App = () => {
 
   return (
     <div>
+      <ToastContainer theme='dark'/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
